6b5b583b454cd36cc6bd07f5b5a5b317026c22db
Johannes Mikulasch <mikulasc@in.tum.de>
2014-11-13 23:25:42 +0100

    Add a PSTH plot function

--- spiketrain.py
+++ spiketrain.py
@@ -54,6 +54,34 @@ def plot(spiketrain):
     plt.show()
 
 
+def plot_psth(spiketrain, binsize=20):
+    """ Plot a Peri-Stimulus Time Histogram """
+
+    import matplotlib.pyplot as plt
+
+    neurons, timesteps = spiketrain.shape
+
+    bins = timesteps // binsize
+
+    fig, axes = plt.subplots(nrows=neurons)
+
+    for i, axis in enumerate(axes):
+        # Histogramm
+        times = np.where(spiketrain[i])[0]
+        axis.hist(times, bins, histtype='bar', stacked=True, fill=True, facecolor='green', alpha=0.5, zorder=0)
+
+        # Scatter (Spikes)
+        y = np.ones(times.shape)
+        axis.scatter(times, y, c='r', s=20, marker="x", zorder=1)
+
+        axis.set_title('Neuron %d' % i)
+        axis.set_ylim(bottom=0)
+        axis.set_xlim(0, timesteps)
+
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
     #s = generate(0.1, (3, 100), [0, 1])
     #s = poisson_homogenous(0.1, 100)
@@ -63,4 +91,4 @@ if __name__ == "__main__":
         s[i] = sound()
 
     print(s)
-    plot(s)
+    plot_psth(s, 40)
