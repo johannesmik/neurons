d3e607d41c037ce494b271ad205f8356835ec9fc
Johannes Mikulasch <mikulasc@in.tum.de>
2015-02-07 22:39:55 +0100

    Edit readthedocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 # Mock imports, so that readthedocs can import them
 import mock
 import sys
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'pylab', 'matplotlib.animation']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
