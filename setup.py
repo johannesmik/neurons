182a0292829d6487367404b1d1542bb615dfeb24
Johannes Mikulasch <mikulasc@in.tum.de>
2015-02-10 10:23:16 +0100

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__ = 'johannes'
+
+import sys
+
+from distutils.core import setup
+
+setup(name='neurons',
+      version='0.3',
+      description='A simple simulation tool for neuron models',
+      author='Johannes Mikulasch',
+      url='http://github.com/johannesmik/neurons',
+      license='BSD',
+      package_dir={'neurons': ''},
+      packages=['neurons']
+      )
